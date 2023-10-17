6b88fb6c1ab2538d3e852cb6dc21bc4b5f565e2e
ecoarchie <eco.artur@gmail.com>
2023-10-18 01:28:18 +0300

    initial commit

---
+++ types.go
@@ -0,0 +1,22 @@
+package main
+
+import "math/rand"
+
+
+type Account struct {
+	ID int `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName string `json:"last_name"`
+	Number int64 `json:"number"`
+	Balance int64 `json:"balance"`
+}
+
+func NewAccount(fName, lName string) *Account {
+	return &Account{
+		ID:        rand.Intn(10000),
+		FirstName: fName,
+		LastName:  lName,
+		Number:    int64(rand.Intn(1000000)),
+		Balance:   0,
+	}
+}
